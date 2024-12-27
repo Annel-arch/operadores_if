f9c77bd1f3e0ae034708642b9c647f208ba85dc2
annel_yopla <annelyoplap@gmail.com>
2024-12-27 14:59:18 -0500

    ejercicio completo

---
+++ ejercicios_operaciones.js
@@ -0,0 +1,45 @@
+// Var, let, const
+
+let nombre= "Annel"
+ nombre = "Annel Yopla";
+
+const nacimiento = "04/11/99"
+console.log (nombre)
+console.log (nacimiento)
+console.log (typeof nombre)
+console.log (typeof nacimiento)
+
+//operadores matemáticos
+
+const suma=4+9
+console.log (suma)
+const resta = 9 - 4
+console.log (resta)
+const multi= 4*9
+console.log (multi)
+const porcentaje = 5 % 2
+console.log (porcentaje)
+
+const edad=24
+const nuevaEdad=edad+1
+console.log (edad)
+console.log (nuevaEdad)
+
+//operadores lógicos
+
+const tipoAnd= 4>3
+console.log (tipoAnd)
+const tipoAnd1= 1>3
+console.log (tipoAnd1)
+// comparación con and &&
+console.log (tipoAnd && tipoAnd1)
+//comparación con or ||
+console.log (tipoAnd || tipoAnd1)
+
+//evaalua y muestra con ||
+const valor = 100 || "no hay money"
+console.log (valor)
+
+// comparacion con not !
+const isRain = false
+console.log (!isRain)
---
+++ if-else.js
@@ -0,0 +1,16 @@
+let nota = 74;
+
+if (nota>0) {
+    if (nota >= 90) {
+        console.log ("Excelente")
+    } else if (nota >=75 && nota <= 89) {
+        console.log ("Bien")
+    } else if (nota >= 60 && nota <75) {
+        console.log ("Suficiente")
+    } else {
+        console.log ("Desaprobado")
+    }
+} else {
+    console.log ("Error en nota")
+}
+
